1e0c2235cace2d7adf1a48a2baed8eb37b29a228
BriandFMarie <briand.marie@live.fr>
2019-11-26 21:43:19 +0100

    initialize node project

---
+++ index.js
@@ -0,0 +1,35 @@
+// Imports
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// To add logger
+const logger = require('./src/config/logger');
+
+// Variables
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+// To parse body of queries
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => logger.info('Connection established successfully'))
+    .then(() => new Promise((resolve, reject) => {
+        app.listen(3000, resolve).on('error', reject);
+    }))
+    .then(() => logger.info(`Server is now running in ${process.env.NODE_ENV || 'development'} on port 3000`))
+    .catch(err => {
+        logger.error(`Error: ${err}`);
+        throw err;
+    });

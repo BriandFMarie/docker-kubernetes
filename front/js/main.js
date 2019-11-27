ea58e369a664e971a417ed1553115508f6b5440d
JohnnyYVARD <johnnyyvard@me.com>
2019-11-27 15:39:49 +0100

    Résolution du problème de pourcentage

--- front/js/main.js
+++ front/js/main.js
@@ -2,24 +2,27 @@ $(document).ready(function() {
     /**
      * Cette fonction ajoute les résultats
      */
-    $(".results").each(function() {
-        $.ajax({
-            url: "http://localhost:3000/api/v1/vote",
-            success: function(data) {
-                let cat = data.result[0].voteCats,
-                    dog = data.result[0].voteDogs;
-                heightDog = (dog / (cat + dog) * 100) * 2;
-                heightCat = (cat / (dog + cat) * 100) * 2;
-                $(".result.cat").each(function() {
-                    $(this).attr("data-height", heightCat).find("span.nbr").html((heightCat / 2) + "%");
-                })
-                $(".result.dog").each(function() {
-                    $(this).attr("data-height", heightDog).find("span.nbr").html((heightDog / 2) + "%");
-                })
+    function getResult() {
+        $(".results").each(function() {
+            $.ajax({
+                url: "http://localhost:3000/api/v1/vote",
+                success: function(data) {
+                    let cat = data.result[0].voteCats,
+                        dog = data.result[0].voteDogs;
+                    heightDog = Math.round((dog / (cat + dog) * 100) * 2);
+                    heightCat = (200 - heightDog > 100) ? 200 - heightDog - 1 : 200 - heightDog;
+                    $(".result.cat").each(function() {
+                        $(this).attr("data-height", heightCat).find("span.nbr").html(Math.ceil((heightCat / 2)) + "%");
+                    })
+                    $(".result.dog").each(function() {
+                        $(this).attr("data-height", heightDog).find("span.nbr").html(Math.ceil((heightDog / 2)) + "%");
+                    })
 
-            }
-        })
-    });
+                }
+            })
+        });
+
+    }
     /**
      * Cette fonction gere le click sur un choix (chien/chat)
      */
@@ -43,6 +46,7 @@ $(document).ready(function() {
      * Cette fonction gère l'affichage d'une step
      */
     function displayStep(step) {
+        getResult();
         $(".container").each(function() {
             $(this).css({ left: (parseInt($(this).attr("data-step")) * 100) + "%" })
         });

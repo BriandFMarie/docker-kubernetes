43a191b693b9bc0b392211612165c8944d45123a
Anatole <anatole.piveteau@imie.fr>
2019-11-28 11:10:07 +0100

    Edit stylesheet Edit JS

--- front/js/main.js
+++ front/js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-    var multiple = 8;
+    var multiple = 4;
+    function animateResult()
+    {
+        $("span.nbr").each(function () {
+            console.log("HERE");
+            if (parseInt($(this).html()) < parseInt($(this).attr("data-percent")))
+                $(this).html(parseInt($(this).html()) + 1);
+        })
+    }
+
     /**
      * Cette fonction ajoute les résultats
      */
     function getResult() {
         $(".results").each(function() {
             $.ajax({
-                url: "http://localhost:3000/api/v1/vote",
+                url: "/api/v1/vote",
                 success: function(data) {
                     let cat = data.result[0].voteCats,
                         dog = data.result[0].voteDogs,
@@ -17,12 +26,11 @@ $(document).ready(function() {
                     heightDog = Math.round((dog / (cat + dog) * 100) * multiple);
                     heightCat = ((100 * multiple) - heightDog > 100) ? (100 * multiple) - heightDog - 1 : (100 * multiple) - heightDog;
                     $(".result.cat").each(function() {
-                        $(this).attr("data-height", heightCat).find("span.nbr").html(Math.ceil((heightCat / multiple)) + "%");
+                        $(this).attr("data-height", heightCat).find("span.nbr").attr("data-percent", Math.ceil((heightCat / multiple)));
                     })
                     $(".result.dog").each(function() {
-                        $(this).attr("data-height", heightDog).find("span.nbr").html(Math.ceil((heightDog / multiple)) + "%");
+                        $(this).attr("data-height", heightDog).find("span.nbr").attr("data-percent", Math.ceil((heightDog / multiple)));
                     })
-
                 }
             })
         });
@@ -44,8 +52,9 @@ $(document).ready(function() {
     function doTransitionOnResult() {
         $(".result").each(function() {
             $(this).height($(this).attr("data-height"));
-            $(this).find("span.nbr").html(parseInt($(this).attr("data-height")) / multiple);
-        })
+            $(this).find("span.nbr").html("0");
+        });
+        setInterval(animateResult, 30);
     }
     /**
      * Cette fonction gère l'affichage d'une step
